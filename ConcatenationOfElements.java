b779646695f03130356b0c82d9b353b5fac097ce
Protocol-Wizard <samakashramesh2007@gmail.com>
2026-02-13 19:49:54 +0530

    Add files via upload

---
+++ ConcatenationOfElements.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+class ConcatenationOfElements {
+    public static void main(String[] args) {
+        char[] charArray = {'H','e','l','l','o'};
+        String str = new String(charArray);
+
+        System.out.println("String: " + str);
+    }
+}
